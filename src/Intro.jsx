2955ee273ab3c0d39e3cbadc22f1a4acb6bb2db2
yashukaSan <ysahu1590@gmail.com>
2026-02-07 14:48:09 +0530

    Added skill icon styling

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,20 +1,57 @@
+import { useState } from 'react'
 
 function Intro(){
+    const [isHover, setIsHover] = useState(false);
+
+
     const skills = [
-        'HTML', 'CSS', 'Tailwind CSS', 'JavaScript',
-        'React.js', "Git / GitHub", 'Responsive Web Design',
-        'Artificial Intelligence', "Technical Communication"
+        {
+            "name": 'HTML',
+            "icon": 'html.png',
+            "class": "h-20 w-20 rounded-full text-shadow m-auto hover:shadow-2xl  hover:shadow-[0px_5px_30px_30px_rgb(256,50,0),inset_2px_10px_1000px_0px_red]"
+        },
+        {
+            "name": 'CSS', 
+            "icon": 'css.png',
+            "class": "h-20 w-20 rounded-full m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(0,100,150),inset_2px_10px_1000px_0px_rgb(0,100,150)]"
+        },
+        {
+            "name": 'Tailwind CSS',
+            "icon": 'tailwind-css.svg',
+            "class": "h-20 w-20 rounded-full m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(0,150,150),inset_2px_10px_100px_0px_rgb(0,150,150)]"
+        },
+        {
+            "name": 'JavaScript',
+            "icon": 'javascript.png',
+            "class": "h-20 w-20 rounded-full m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(256,200,0),inset_2px_10px_100px_0px_rgb(256,200,0)]"
+        },
+        {
+            "name": 'React.js',
+            "icon": 'react.png',
+            "class": "h-20 w-20 rounded-full m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(0,250,250),inset_2px_10px_100px_0px_rgb(0,250,250)]"
+        },
+        {
+            "name": "Git / GitHub",
+            "icon": 'git.svg',
+            "class": "h-20 w-20 rounded-full m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(256,50,0),inset_2px_10px_100px_0px_black]"
+        },
+        {
+            "name": 'Responsive Web Design',
+            "icon": 'responsive-web.png',
+            "class": "h-20 w-20 rounded-full m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(6,150,200),inset_1px_10px_20px_12px_,inset_100px_2px_5px_5px_violet]"
+        },
+        {
+            "name": 'Artificial Intelligence',
+            "icon": 'ai.png',
+            "class": "h-20 w-20 rounded-full p-1 m-auto hover:shadow-2xl hover:shadow-[0px_5px_30px_30px_rgb(156,150,200),inset_0px_0px_15px_10px_pink,inset_2px_30px_200px_20px_lightblue]"
+        }
     ];
 
-    const iconsName = [
-        'html.png', 'css.png', 'tailwind-css.svg', 'javascript.png',
-        'react.png', 'git.svg', 'responsive-web.png', 'ai.png'
-    ];
 
     const contacts = [
         ["Phone", "+91 8982199384"],
         ["Address", "Datia, Madhya Pradesh, India"], 
-        ["LinkedIn", "linkedin/in/yogesh-s-12637232b/"],
+        ["LinkedIn", "linkedin.com/in/yogesh-s-12637232b/"],
         ["GitHub", "github.com/yashukaSan"]
     ];
     
@@ -62,19 +99,29 @@ function Intro(){
                         </p>
                 </div>
             </div>
-            <img src=".\src\assets\yogesh_pic.jpg" alt="Yogesh Pic" className="rounded-full w-45 " />
+
         </div>
         <hr className="" />
         {/* Skill Section */}
         <div className="border-3 my-4 p-4">
             <h3>SKILLS</h3>
-            <ul className="grid grid-cols-3">
+            <ul className="grid xs:grid-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-9 gap-4 justify-around">
                 {skills.map((skill, index)=>{
-                    const path = `./src/assets/icons/${iconsName[index]}`;
+                    const path = `./src/assets/icons/${skill['icon']}`;
                     return (
-                    <li key={index}>
-                        {skill}
-                        <img src={path} alt={skill} />
+                    <li key={index} className="m-0">
+                        <img
+                         onMouseEnter={()=>{setIsHover(true)}}
+                         onMouseLeave={()=>{setIsHover(false)}}
+                         src={path} alt={skill['name']}
+                         className={skill['class']} 
+                        />
+                        {
+                        isHover && (
+                            <p className="text-center mt-0 text-shadow-red-400 text-shadow-lg">
+                                {skill['name']}
+                            </p>)
+                        }
                     </li>
                 )})}
             </ul>
